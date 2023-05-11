24388b1fb8f073b464043eb9386df9f5da66808c
unknown <bharath-4143>
2023-05-11 16:21:59 +0530

    add pages

--- index.js
+++
@@ -1,9 +0,0 @@
-function opennav(){
-    document.getElementById("nav").style.visibility="visible"
-}
-function closenav(){
-    document.getElementById("nav").style.visibility="hidden"
-}
-function openconnect(){
-    document.getElementById("connectbar").style.zIndex
-}
