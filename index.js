72fdcc8c704db19af277589ea5f345a637a530cd
unknown <bharath-4143>
2023-05-11 09:31:24 +0530

    compressed png

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 function opennav(){
     document.getElementById("nav").style.visibility="visible"
-    document.getElementById("nav").style.transition= 0.5;"vertical"
 }
 function closenav(){
     document.getElementById("nav").style.visibility="hidden"
+}
+function openconnect(){
+    document.getElementById("connectbar").style.zIndex
 }
